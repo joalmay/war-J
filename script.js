1e99d8e49fe2ded2d48760b7869b152cef10cc5a
joalmay <joalmay@gmail.com>
2014-01-22 20:17:05 -0700

    Initial
    Signed-off-by: joalmay <joalmay@gmail.com>

---
+++ script.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+
+	//what does this do?
+	function convert_value_to_string(value) {
+		if (value > 10) {
+			switch (value) {
+				case 11:
+				return 'Jack';
+				break;
+				case 12:
+				return 'Queen';
+				break;
+				case 13:
+				return 'King';
+				break;
+			}
+		}
+		return value.toString();
+	}
+
+	//what does this do?
+	var deck = [];
+	var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+	for (var i = 0; i<suits.length; i++) {
+		var suit = suits[i];
+		for (var j = 0; j<13; j++) {
+			deck.push({number: j+1, suit: suit});
+		}
+	}
+	
+	//shuffle the deck
+	
+	
+	var cards_player_1 = [];
+	var cards_player_2 = [];
+	//divide out the cards into the two arrays
+	
+	
+	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
+	function war() {
+	}
+	
+	
+	//create a play function
+		//compare the cards
+		//give the winner both cards (at end of deck)
+	function play() {
+		
+		//this function (defined below) will continue to the next turn
+		advance();
+	}
+	
+	function advance() {
+		//take the top two cards and display them
+		if (cards_player_1.length) {
+			var card_1 = cards_player_1[0];
+			var card_2 = cards_player_2[0];
+			$("#opp-card").html(convert_value_to_string(card_1.number)+" "+card_1.suit);
+			$("#opp-card-count").html(cards_player_1.length);
+			$("#my-card").html(convert_value_to_string(card_2.number)+" "+card_2.suit);
+			$("#my-card-count").html(cards_player_2.length);
+			
+		}
+	}
+	advance();
+	
+	$(".btn").click(function() {
+		play();
+	});
+});
